5d3024f1f6fc3ad49441d5a13806a759ba0ad7d7
7ng6t2snvn-lan <7ng6t2snvn@privaterelay.appleid.com>
2025-12-06 20:13:15 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+// Gelato Formulator Service Worker v3.0.0
+const CACHE_VERSION = 'gelato-v3.0.0';
+const CACHE_NAME = CACHE_VERSION;
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.error('Cache failed:', err))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  if (!event.request.url.startsWith(self.location.origin)) return;
+  
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
+  );
+});
